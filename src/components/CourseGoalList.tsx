5d1cbd5209fd7f832813e70abf89f285bb4e0407
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2024-03-16 08:35:39 -0700

    fist commit

---
+++ src/components/CourseGoalList.tsx
@@ -0,0 +1,21 @@
+import CourseGoal from "./CourseGoal"
+import { type CourseGoal as CourseGoalProps } from "../App"
+
+type CourseGoalListProps = {
+  goals: CourseGoalProps[]
+  onDeleteGoal: (id: number) => void
+}
+
+export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
+  return (
+    <ul>
+      {goals.map(({ title, description, id }) => (
+        <li key={id}>
+          <CourseGoal title={title} id={id} onDelete={onDeleteGoal}>
+            <p>{description}</p>
+          </CourseGoal>
+        </li>
+      ))}
+    </ul>
+  )
+}
---
+++ src/components/NewGoal.tsx
@@ -0,0 +1,38 @@
+import { useRef, type FormEvent } from "react"
+
+type NewGoalProps = {
+  onAddGoal: ({ title, description }: { title: string, description: string }) => void
+}
+
+export default function NewGoal({ onAddGoal }: NewGoalProps) {
+
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    onAddGoal({
+      title: goal.current!.value,
+      description: summary.current!.value
+    })
+
+    event.currentTarget.reset()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="goal">Your Goal</label>
+        <input id="goal" type="text" name="goal" ref={goal} />
+      </p>
+      <p>
+        <label htmlFor="summary">Short Summary</label>
+        <input id="summary" type="text" name="summary" ref={summary} />
+      </p>
+      <p>
+        <button>Add Goal</button>
+      </p>
+    </form>
+  )
+}
